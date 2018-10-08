8da609566f25501b3d1f8eca661fcf0d80bda0b5
Jonathan Andres Jimenez Trujillo <32826431+jonajitru781@users.noreply.github.com>
2018-10-07 21:07:33 -0500

    Factura

---
+++ src/Factura.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jonaj
+ */
+import java.util.HashMap;
+
+public class Factura {
+    private String cliente;
+    private  HashMap<Producto, Integer> productos;
+    
+    public Factura(String cliente){
+        this.cliente= cliente;
+        this.productos=new HashMap<>();
+    }
+    
+    public HashMap<Producto, Integer> getProductos() {
+        return productos;
+    }
+
+    public boolean addProducto(Producto producto, int a) {
+        this.precio+=producto.getPrecioPU()*a;
+        return productos.put(producto,a)==null;
+    }
+    
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+}
